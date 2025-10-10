08c5bc932c15e90e15ba2691b5122313069dae70
70792497+eksql010@users.noreply.github.com <70792497+eksql010@users.noreply.github.com>
2025-10-10 18:26:54 +0900

    FEAT : 파티클 시스템, 이미터 클래스 추가 및 이펙트 툴 imGui Widget 추가 (위젯 기능 작성 중)

--- Editor/Public/Level_Editor.h
+++ Editor/Public/Level_Editor.h
@@ -17,6 +17,31 @@ public:
 	virtual HRESULT Render() override;
 
 private:
+	// Effect Tool Test
+
+	// ===== GUI 관련 변수 =====
+	_char m_szSystemName[64] = {};
+	vector<class CParticleSystem*> m_ParticleSystems;
+	_int m_iSelectedSystem = { -1 };
+	_int m_iSelectedEmitter = { -1 };
+
+	// ===== 파티클 관련 기본 변수 =====
+	_float3		m_vCenter = {};
+	_float3		m_vDir = { 0.f, 1.f, 0.f };
+	_float		m_fSpeed = {};
+	_float		m_fScale = { 1.f };
+	_float		m_fLifeTime = {};
+	_bool		m_bLoop = {};
+	_int		m_iNumInstance = {};
+
+	// ===== 파티클 관련 확장 변수 =====
+	_bool		m_bUseRandomDir = {};
+	_float		m_fAngleMin = {};
+	_float		m_fAngleMax = {};
+	_bool		m_bUseGravity = {};
+	_float3		m_vGravity = {};
+
+private:
 	HRESULT Ready_Layer_BackGround(const _wstring& strLayerTag);
 
 public:
