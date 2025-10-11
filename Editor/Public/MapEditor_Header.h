823fed2fafc118a3ae98e38e2bd16d17fd283ce4
zuuun-0 <wnsdud1832@gmail.com>
2025-10-11 21:12:02 +0900

    FEAT: Editor -> MapObject 관련 클래스들 생성, MeshInstance용 쉐이더 추가, MapLevel 코드 추가 중
    Model_Instance, Mesh_Instance 클래스 코드 수정 진행 중 ( Add, Fix )

---
+++ Editor/Public/MapEditor_Header.h
@@ -0,0 +1,11 @@
+#ifndef MapEditor_Header_h__
+#define MapEditor_Header_h__
+
+#include "Camera_Map.h"
+
+#include "Prop_Static.h"
+#include "Prop_Animated.h"
+#include "Prop_Interactive.h"
+#include "Prop_Destructible.h"
+
+#endif // !MapEditor_Header_h__
