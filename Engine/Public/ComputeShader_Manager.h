486d4135f676d18f007128888f5de9136e588e5d
70792497+eksql010@users.noreply.github.com <70792497+eksql010@users.noreply.github.com>
2025-10-13 16:51:19 +0900

    FEAT : Compute Shader, Compute Shader Manager 추가

---
+++ Engine/Public/ComputeShader_Manager.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Base.h"
+
+NS_BEGIN(Engine)
+
+class CComputeShader_Manager final : public CBase
+{
+public:
+	typedef struct tagComputeJobDesc
+	{
+		class CComputeShader* pShader = { nullptr };
+		COMPUTE_PASS_DESC PassDesc;
+	}COMPUTE_JOB_DESC;
+
+private:
+	CComputeShader_Manager();
+	virtual ~CComputeShader_Manager() = default;
+
+public:
+	HRESULT		Initialize();
+
+public:
+	void		Add_Job(COMPUTEJOP eJobTag, const COMPUTE_JOB_DESC& Desc, _bool isExecuteNow);
+	void		Execute_Job(COMPUTEJOP eJobTag);
+
+private:
+	vector<vector<COMPUTE_JOB_DESC>>	m_Jobs;
+
+public:
+	static CComputeShader_Manager*		Create();
+	virtual void						Free() override;
+};
+
+NS_END
