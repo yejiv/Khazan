2093bbee298d790f9c142ec4d2642f2c5a6a8215
zuuun-0 <wnsdud1832@gmail.com>
2025-10-14 13:34:30 +0900

    FEAT: MapEdiotr에서 JSON 읽어서 Prototype 생성, AddLayer 기능 추가 중

--- Editor/Public/Editor_Defines.h
+++ Editor/Public/Editor_Defines.h
@@ -7,8 +7,8 @@ namespace Editor
 	const unsigned int			g_iWinSizeX = 1920;
 	const unsigned int			g_iWinSizeY = 1080;
 
-	const unsigned int			g_iWinSizeX_Imgui = 800;
-	const unsigned int			g_iWinSizeY_Imgui = 600;
+	const unsigned int			g_iWinSizeX_Imgui = 1600;
+	const unsigned int			g_iWinSizeY_Imgui = 900;
 
 	enum class LEVEL { STATIC, LOADING, EDITOR, MAP, ANIMATION, EFFECT, UI, END };
 	enum class COLLISION_LAYER { MAP, PLAYER, MONSTER, ITEM, EFFECT, SKILL, END };
