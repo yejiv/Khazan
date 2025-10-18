d833f9bee90f361f254f3b138bcf660c3fec572e
yetak <yeji4903@naver.com>
2025-10-18 09:33:19 +0900

    feat : Effect Mesh Fix

---
+++ Editor/Private/Effect_Mesh_Instance.cpp
@@ -0,0 +1,272 @@
+﻿#include "Effect_Mesh_Instance.h"
+#include "Effect_Prefab.h"
+#include "GameInstance.h"
+
+CEffect_Mesh_Instance::CEffect_Mesh_Instance(ID3D11Device* pDevice, ID3D11DeviceContext* pDeviceContext)
+    : CEffect_Element{pDevice, pDeviceContext}
+{
+}
+
+CEffect_Mesh_Instance::CEffect_Mesh_Instance(const CEffect_Mesh_Instance& Prototype)
+    : CEffect_Element(Prototype)
+{
+}
+
+HRESULT CEffect_Mesh_Instance::Initialize_Clone(void* pArg)
+{
+    __super::Initialize_Clone(pArg);
+
+    if (FAILED(Ready_Component()))
+        return E_FAIL;
+
+    Apply(pArg);
+
+    return S_OK;
+}
+
+void CEffect_Mesh_Instance::Priority_Update(_float fTimeDelta)
+{
+    __super::Priority_Update(fTimeDelta);
+}
+
+void CEffect_Mesh_Instance::Update(_float fTimeDelta)
+{
+    for(auto it = m_TimeTracks.begin(); it != m_TimeTracks.end();)
+    {
+        it->fCurTime += fTimeDelta;
+
+        if(it->fCurTime > it->fDurTime && it->EventType != 0)
+        {
+            dynamic_cast<CVIBuffer_Mesh_Instance*>(m_pVIBufferCom)->Remove_Speed(CVIBuffer_Mesh_Instance::SPEED_VALUE(it->EventType - 1));
+            it = m_TimeTracks.erase(it);
+        }
+        else
+            ++it;
+    }
+
+    m_fCurTime += fTimeDelta;
+
+    m_pVIBufferCom->Update(fTimeDelta);
+    __super::Update(fTimeDelta);
+
+    /* Edit */
+    if (m_TimeTracks.size() == 0)
+    {
+        m_pVIBufferCom->Remove_Speed();
+        m_fCurTime = 0.f;
+    }
+}
+
+void CEffect_Mesh_Instance::Late_Update(_float fTimeDelta)
+{
+    __super::Late_Update(fTimeDelta);
+}
+
+HRESULT CEffect_Mesh_Instance::Render()
+{
+    if (FAILED(Bind_ShaderResources()))
+        return E_FAIL;
+
+    //m_pShaderCom->Begin((_uint)m_Data.TextureBindType);
+    m_pShaderCom->Begin(0);
+
+    m_pVIBufferCom->Bind_Resources();
+
+    m_pVIBufferCom->Render();
+
+    return S_OK;
+}
+
+void CEffect_Mesh_Instance::Save_Data(ofstream& os)
+{
+}
+
+void CEffect_Mesh_Instance::Edit_Element()
+{
+    _int            isCircle = (_int)m_sEditingData.IsCircle;
+
+    ImGui::RadioButton("Spawn_BoundingBox", &isCircle, 0);
+    ImGui::RadioButton("Spawn_Circle", &isCircle, 1);
+
+    if(isCircle == 0)
+    {
+        ImGui::InputFloat3("Center : ",reinterpret_cast<_float*>(&m_sEditingData.vCenter));
+        ImGui::InputFloat3("Range : ",reinterpret_cast<_float*>(&m_sEditingData.vRange));
+    }
+    else 
+        ImGui::InputFloat("Circle Offset  : ",&m_sEditingData.fOffset); 
+    
+    ImGui::InputScalar("Instance Num : ", ImGuiDataType_U32, &m_sEditingData.iNumInstance);
+
+    ImGui::InputFloat2("Size : ", reinterpret_cast<_float*>(&m_sEditingData.vSize));
+    ImGui::InputFloat("Size Ratio : ", &m_sEditingData.fSizeRatio);
+    ImGui::InputFloat2("LifeTime : ", reinterpret_cast<_float*>(&m_sEditingData.vLifeTime));
+    ImGui::InputFloat2("Scrolling Speed : ", reinterpret_cast<_float*>(&m_sEditingData.iScrollSpeed));
+
+    ImGui::ColorEdit4("MyColorWithAlpha",(float*)&m_sEditingData.vColor);
+
+    const char* textures[] = { "test0", "test1", "test2",  "test3",  "test4",  "test5",  "test6" ,  "test7" ,  "test8" ,  "test9" ,  "test10" ,  "test11" ,  "test12" ,  "test13",  "test14" ,  "test15" ,  "test16" ,  "test17" ,  "test18" ,  "test19" ,  "test20" ,  "test21",  "test22" ,  "test23"};
+    ImGui::ListBox("Mesh Textures", reinterpret_cast<int*>(&m_sEditingData.iTextureIdx), textures, IM_ARRAYSIZE(textures));
+
+    const char* Meshes[] = { "Mesh1", "Mesh2", "Mesh3",  "Mesh4",  "Mesh5",  "Mesh6" };
+    ImGui::ListBox("Mesh Shape", reinterpret_cast<int*>(&m_sEditingData.iMeshTypeIdx), Meshes, IM_ARRAYSIZE(Meshes));
+
+    m_sEditingData.IsCircle = isCircle;
+
+    if (ImGui::Button("Apply"))
+        Apply(&m_sEditingData);
+    if (ImGui::Button("Revert"))
+        RevertChanges();
+}
+
+void CEffect_Mesh_Instance::RevertChanges()
+{
+    m_sEditingData = m_sData;
+}
+
+void CEffect_Mesh_Instance::Reset()
+{
+    __super::Reset();
+    m_pVIBufferCom->Reset();
+}
+
+void CEffect_Mesh_Instance::SetSpreadData(void* pArg)
+{
+    CEffect_Prefab::EFFECT_EVENT data = *static_cast<CEffect_Prefab::EFFECT_EVENT*>(pArg);
+    m_pVIBufferCom->Setting_Speed(CVIBuffer_Mesh_Instance::SPEED_VALUE::SPREAD_SPEED, data.fSpreadSpeed);
+    m_pVIBufferCom->Setting_Pivot(data.fPivot);
+    SetData(ENUM_CLASS(data.eEventType), data.fDuration); 
+}
+
+void CEffect_Mesh_Instance::SetRotateData(void* pArg)
+{
+    CEffect_Prefab::EFFECT_EVENT data = *static_cast<CEffect_Prefab::EFFECT_EVENT*>(pArg);
+    m_pVIBufferCom->Setting_Speed(CVIBuffer_Mesh_Instance::SPEED_VALUE::ROTATION_SPEED, data.fRotationSpeed);
+    m_pVIBufferCom->Setting_Pivot(data.fPivot);
+    SetData(ENUM_CLASS(data.eEventType),data.fDuration);
+}
+
+void CEffect_Mesh_Instance::SetTwinkleData(void* pArg)
+{
+    CEffect_Prefab::EFFECT_EVENT data = *static_cast<CEffect_Prefab::EFFECT_EVENT*>(pArg);
+    m_pVIBufferCom->Setting_Speed(CVIBuffer_Mesh_Instance::SPEED_VALUE::SCALE_SPEED, data.fScaleSpeed);
+    SetData(ENUM_CLASS(data.eEventType),data.fDuration);
+}
+
+void CEffect_Mesh_Instance::SetUpwardData(void* pArg)
+{
+    CEffect_Prefab::EFFECT_EVENT data = *static_cast<CEffect_Prefab::EFFECT_EVENT*>(pArg);
+    m_pVIBufferCom->Setting_Speed(CVIBuffer_Mesh_Instance::SPEED_VALUE::UPWARD_SPEED, data.fUpwardSpeed);
+    SetData(ENUM_CLASS(data.eEventType),data.fDuration);
+}
+
+void CEffect_Mesh_Instance::SetScrollData(void* pArg)
+{
+    m_fScrollSpeed.x = *static_cast<_float*>(pArg);
+    m_fScrollSpeed.y = *(static_cast<_float*>(pArg) + 1);
+}
+
+HRESULT CEffect_Mesh_Instance::Ready_Component()
+{
+    if (FAILED(CGameObject::Add_Component(ENUM_CLASS(LEVEL::STATIC), TEXT("Prototype_Component_Shader_VtxInstance_Particle"),
+        TEXT("Com_Shader"), reinterpret_cast<CComponent**>(&m_pShaderCom), nullptr)))
+        return E_FAIL;
+
+    if (FAILED(CGameObject::Add_Component(ENUM_CLASS(LEVEL::STATIC), TEXT("Prototype_Component_Texture_Slash"),
+        TEXT("Com_Texture"), reinterpret_cast<CComponent**>(&m_pTextureCom), nullptr)))
+        return E_FAIL;
+
+    if (FAILED(CGameObject::Add_Component(ENUM_CLASS(LEVEL::STATIC), TEXT("Prototype_Component_Texture_MeshEffect_Masking"),
+        TEXT("Com_TextureMask"), reinterpret_cast<CComponent**>(&m_pMaskTextureCom), nullptr)))
+        return E_FAIL;
+
+    return S_OK;
+}
+
+HRESULT CEffect_Mesh_Instance::Bind_ShaderResources()
+{
+    if (FAILED(m_pShaderCom->Bind_Matrix("g_WorldMatrix", &m_CombinedWorldMatrix)))
+        return E_FAIL;
+
+    if (FAILED(m_pShaderCom->Bind_Matrix("g_ViewMatrix", m_pGameInstance->Get_Transform_Float4x4(D3DTS::VIEW))))
+        return E_FAIL;
+
+    if (FAILED(m_pShaderCom->Bind_Matrix("g_ProjMatrix", m_pGameInstance->Get_Transform_Float4x4(D3DTS::PROJ))))
+        return E_FAIL;
+
+    if(FAILED(m_pShaderCom->Bind_RawValue("g_vSourceColor", &m_sEditingData.vColor, sizeof(_float4))))
+        return E_FAIL;
+
+    if (FAILED(m_pShaderCom->Bind_RawValue("g_RunningTime", &m_fCurTime, sizeof(_float))))
+        return E_FAIL;
+
+    if (FAILED(m_pShaderCom->Bind_RawValue("g_ScrollSpeed", &m_fScrollSpeed, sizeof(_float2))))
+        return E_FAIL;
+
+    if (FAILED(m_pTextureCom->Bind_Shader_Resource(m_pShaderCom, "g_DiffuseTexture", m_sData.iTextureIdx)))
+        return E_FAIL;
+
+    if (FAILED(m_pMaskTextureCom->Bind_Shader_Resource(m_pShaderCom, "g_MaskTexture", 0)))
+        return E_FAIL;
+
+    return S_OK;
+}
+
+void CEffect_Mesh_Instance::Apply(void* pArg)
+{
+    m_sData = *static_cast<PARTICLE_DESC*>(pArg);
+    const char* format = "../../Client/Bin/Resources/Model/Data/untitled%d_Mesh.dat";
+
+    char finalPathBuffer[MAX_PATH] = {};
+    sprintf_s(finalPathBuffer, MAX_PATH, format, m_sData.iMeshTypeIdx);
+    m_sData.pFilePath = finalPathBuffer;
+
+    Safe_Release(m_pVIBufferCom);
+    m_pVIBufferCom = CVIBuffer_Mesh_Instance::Create(m_pDevice, m_pContext, &m_sData);
+    m_pVIBufferCom->Initialize_Clone(nullptr);
+    m_iEffect_Type = 0;
+
+    m_sEditingData = m_sData;
+    m_fScrollSpeed = m_sData.iScrollSpeed;
+
+    m_fCurTime = 0.f;
+}
+
+CEffect_Mesh_Instance* CEffect_Mesh_Instance::Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext, void* pArg)
+{
+    CEffect_Mesh_Instance* pInstance = new CEffect_Mesh_Instance(pDevice, pContext);
+
+    if (FAILED(pInstance->Initialize_Clone(pArg)))
+    {
+        MSG_BOX(TEXT("Failed to Created : CEffect_Mesh_Instance"));
+        Safe_Release(pInstance);
+    }
+
+    return pInstance;
+}
+
+CGameObject* CEffect_Mesh_Instance::Clone(void* pArg)
+{
+    CEffect_Mesh_Instance* pInstance = new CEffect_Mesh_Instance(*this);
+
+    if (FAILED(pInstance->Initialize_Clone(pArg)))
+    {
+        MSG_BOX(TEXT("Failed to Created : CEffect_Mesh_Instance"));
+        Safe_Release(pInstance);
+    }
+
+    return pInstance;
+}
+
+void CEffect_Mesh_Instance::Free()
+{
+    __super::Free();
+
+    Safe_Release(m_pTextureCom);
+    Safe_Release(m_pMaskTextureCom);
+    Safe_Release(m_pVIBufferCom);
+}
+
+
+
+
---
+++ Editor/Public/Effect_Mesh_Instance.h
@@ -0,0 +1,73 @@
+﻿#pragma once
+#include "Editor_Defines.h"
+#include "Effect_Element.h"
+#include "VIBuffer_Mesh_Instance.h"
+
+NS_BEGIN(Engine)
+class CTexture;
+class CVIBuffer_Mesh_Instance;
+NS_END
+
+NS_BEGIN(Editor)
+
+class CEffect_Mesh_Instance : public CEffect_Element
+{
+public:
+	typedef struct tagParticlePrototypeDesc : public CVIBuffer_Mesh_Instance::POINT_MESH_DESC
+	{
+		_float4		vColor;
+		_uint		iTextureIdx;
+		_uint		iMeshTypeIdx;
+		_float2		iScrollSpeed;
+	}PARTICLE_DESC;
+
+private:
+	CEffect_Mesh_Instance(ID3D11Device* pDevice, ID3D11DeviceContext* pDeviceContext);
+	CEffect_Mesh_Instance(const CEffect_Mesh_Instance& Prototype);
+	virtual ~CEffect_Mesh_Instance() = default;
+
+public:
+	virtual HRESULT					Initialize_Clone(void* pArg) override;
+	virtual void					Priority_Update(_float fTimeDelta) override;
+	virtual void					Update(_float fTimeDelta) override;
+	virtual void					Late_Update(_float fTimeDelta) override;
+	virtual HRESULT					Render() override;
+
+public:
+	virtual void					Save_Data(ofstream& os);
+	virtual void					Edit_Element() override;
+	virtual void					RevertChanges() override;
+	virtual void					Reset() override;
+
+	virtual void					SetSpreadData(void* pArg) override;
+	virtual void					SetRotateData(void* pArg) override;
+	virtual void					SetTwinkleData(void* pArg) override;
+	virtual void					SetUpwardData(void* pArg) override;
+	virtual void					SetScrollData(void* pArg) override;
+
+private:
+	HRESULT							Ready_Component() override;
+	HRESULT							Bind_ShaderResources() override;
+	virtual void					Apply(void* pArg) override;
+
+private:
+	CTexture*						m_pTextureCom = { nullptr };
+	CTexture*						m_pMaskTextureCom = { nullptr };
+	CVIBuffer_Mesh_Instance*		m_pVIBufferCom = { nullptr };
+
+private :
+	PARTICLE_DESC					m_sData;
+	PARTICLE_DESC					m_sEditingData;
+	_float							m_fCurTime;
+
+public:
+	static CEffect_Mesh_Instance*	Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext, void* pArg);
+	virtual CGameObject*			Clone(void* pArg);
+	virtual void					Free() override;
+
+};
+
+NS_END
+
+
+
