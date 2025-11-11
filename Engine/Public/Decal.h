dcd997e44b2a5409e1eed49196584b835ec90098
Danboo <dleksql214@naver.com>
2025-11-11 20:33:12 +0900

    FEAT : 데코레이션용 데칼 셰이더 패스 추가 및 변수 추가

--- Engine/Public/Decal.h
+++ Engine/Public/Decal.h
@@ -34,11 +34,16 @@ public:
 	_uint					Get_RandomSeed() { return m_iRandSeed; }
 	void					Set_RandomSeed(_uint iSeed) { m_iRandSeed = iSeed; }
 
+    // 单内饭捞记侩 单漠
+    void                    Set_EnableDecoration(_bool isEnable) { m_isDecoration = isEnable; }
+
 private:
 	_float					m_fTimeAcc = {};
 	_float					m_fOpacity = {};
 	_uint					m_iTextureIndex = {};
 	_uint					m_iRandSeed = {};
+
+    _bool                   m_isDecoration = {};
 
 	DECAL_DESC				m_Desc = {};
 
