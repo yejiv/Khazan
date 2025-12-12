df1a9e85ebdbc2021852c5f4471d3718e5559d48
kimbumsu0312 <91525897+kimbumsu0312@users.noreply.github.com>
2025-12-13 01:38:08 +0900

    feat:엘리트 몬스터 어나운스 추가, 할버드 브루탈 어택 수정

--- Client/Private/Level_HeinMach.cpp
+++ Client/Private/Level_HeinMach.cpp
@@ -732,9 +732,8 @@ HRESULT CLevel_HeinMach::Ready_Layer_Monster_SubLV(const _wstring& strLayerTag, 
         {
             CMonster::MONSTER_DESC MonsterDesc{};
             MonsterDesc.fAttack = 150.f;
-            MonsterDesc.fMaxHP = 4500.f;
- //           MonsterDesc.fMaxStamina = 450.f;
-            MonsterDesc.fMaxStamina = 50.f;
+            MonsterDesc.fMaxHP = 5500.f;
+            MonsterDesc.fMaxStamina = 350.f;
             MonsterDesc.fMoveSpeed = 10.f;
             MonsterDesc.fSpeedPerSec = 3.f;
             MonsterDesc.fRotationPerSec = 180.f;
