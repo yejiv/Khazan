87967b1235b2670e06d8ab97207f122c0e7154c4
kwon7330 <144770981+kwon7330@users.noreply.github.com>
2025-12-13 17:24:40 +0900

    FEAT : 예투가 QA : 브레스 작업, 죽었는데 가드 판정, 슬립 상태에서 히트 처리, 뎀프시롤 디테일

--- Client/Private/AS_Dead_Imp_Melee.cpp
+++ Client/Private/AS_Dead_Imp_Melee.cpp
@@ -5,6 +5,7 @@
 #include "BlackBoard.h"
 #include "AI_Controller.h"
 #include "Interaction_Item.h"
+#include "Imp_Sword.h"
 
 CAS_Dead_Imp_Melee::CAS_Dead_Imp_Melee()
 {
@@ -16,6 +17,11 @@ void CAS_Dead_Imp_Melee::Enter(CStateMachine* pFSM, CGameObject* pOwner)
     CModel* pModel = static_cast<CModel*>(pImp->Get_Body()->Get_Component(TEXT("Com_Model")));
 
     pModel->Set_Animation(10);
+
+    pImp->Get_Sword()->Set_OnAttackCollision(false);
+    CInteraction_Item* pItem = dynamic_cast<CInteraction_Item*>(m_pGameInstance->Pop_PoolObject(m_pGameInstance->Get_CurrentLevelID(), TEXT("Item")));
+    pItem->RandNormal_Item(pOwner->Get_Transform()->Get_State(STATE::POSITION));
+    m_pGameInstance->Push_PoolObject_ToLayer(m_pGameInstance->Get_CurrentLevelID(), TEXT("Layer_Item"), pItem);
 }
 
 void CAS_Dead_Imp_Melee::Update(CStateMachine* pFSM, CGameObject* pOwner, _float fTimeDelta)
