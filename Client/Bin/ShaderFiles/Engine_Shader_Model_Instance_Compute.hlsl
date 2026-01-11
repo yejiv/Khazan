6e04f179b94b726a3f920ca47f35fc6df72408d9
lasval <lasval39@gmail.com>
2026-01-11 18:57:12 +0900

    MERGE

--- Client/Bin/ShaderFiles/Engine_Shader_Model_Instance_Compute.hlsl
+++ Client/Bin/ShaderFiles/Engine_Shader_Model_Instance_Compute.hlsl
@@ -233,26 +233,32 @@ void CS_RESET(uint3 DTid : SV_DispatchThreadID)
     g_SpeedData[iIndex] = SpeedData;
     g_SpeedData[0].bDead = 1.f;
 }
-
 [numthreads(256, 1, 1)]
 void CS_TURBULENCE(uint3 DTid : SV_DispatchThreadID)
 {
     uint iIndex = DTid.x;
     if (iIndex >= g_iNumInstances)
         return;
-
+    
     VTXINSTANCE_PARTICLE Particle = g_OutputData[iIndex];
     float3 pos = Particle.vTranslation.xyz;
 
-    float3 noise;
-    noise.x = g_NoiseTexture.SampleLevel(g_LinearWrapSampler, pos.yz * g_TurblunceSampleSize, 0).r * 2.0f - 1.0f;
-    noise.y = g_NoiseTexture.SampleLevel(g_LinearWrapSampler, pos.xz * g_TurblunceSampleSize, 0).r * 2.0f - 1.0f;
-    noise.z = g_NoiseTexture.SampleLevel(g_LinearWrapSampler, pos.xy * g_TurblunceSampleSize, 0).r * 2.0f - 1.0f;
+    // 방향 편향 방지 ===
+    float2 offset1 = float2(sin(g_TotalTime), cos(g_TotalTime)) * 0.5f;
+    float2 offset2 = float2(sin(g_TotalTime * 1.37f), cos(g_TotalTime * 1.91f)) * 0.5f;
+    float2 offset3 = float2(sin(g_TotalTime * 0.77f), cos(g_TotalTime * 1.21f)) * 0.5f;
+    
+    float forceX = (g_NoiseTexture.SampleLevel(g_LinearWrapSampler, pos.yz * g_TurblunceSampleSize + offset1, 0).r - 0.5f) * 2.f;
+    float forceY = (g_NoiseTexture.SampleLevel(g_LinearWrapSampler, pos.xz * g_TurblunceSampleSize + offset2, 0).r - 0.5f) * 2.f;
+    float forceZ = (g_NoiseTexture.SampleLevel(g_LinearWrapSampler, pos.xy * g_TurblunceSampleSize + offset3, 0).r - 0.5f) * 2.f;
 
-    Particle.vTranslation.xyz += noise * g_TurblunceSpeed * g_fTimeDelta;
+    float3 noiseDir = normalize(float3(forceX, forceY, forceZ));
+
+    Particle.vTranslation.xyz += noiseDir * g_TurblunceSpeed * g_fTimeDelta;
 
     g_OutputData[iIndex] = Particle;
 }
+
 
 [numthreads(256, 1, 1)]
 void CS_RESET_DEAD_FLAG(uint3 DTid : SV_DispatchThreadID)
